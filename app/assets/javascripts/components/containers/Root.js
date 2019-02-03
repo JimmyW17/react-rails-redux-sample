b76ac3a6567e2847fc01f716f16c8c3de21c26f3
Jimmy <wujimmy94@gmail.com>
2019-02-03 14:01:28 -0500

    added comments

--- app/assets/javascripts/components/containers/Root.js
+++ app/assets/javascripts/components/containers/Root.js
@@ -9,13 +9,16 @@ const store = configureStore();
 
 export default class Root extends Component {
   componentWillMount() {
-    store.dispatch(setCounter(this.props.counter));
+    store.dispatch(setCounter(this.props.counter)); // initialize states using dispatch
   }
   render() {
     return (
+      // Provider makes the store available to all container components in the application without passing it explicitly
+      // You only need to use it once when you render the root component
       <Provider store={store}>
         <CounterApp />
       </Provider>
     );
   }
 }
+
--- app/assets/javascripts/components/store/configureStore.js
+++ app/assets/javascripts/components/store/configureStore.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import thunk from 'redux-thunk'; // allows simple asynchronous use of dispatch
 import rootReducer from '../reducers';
 
+// Middleware in redux is mostly used to allow dispatching async actions
 const createStoreWithMiddleware = applyMiddleware(
   thunk
 )(createStore);
