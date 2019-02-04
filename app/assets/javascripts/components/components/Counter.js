b52644ca2a031183ded4eff8b9fb4921763d4fbc
Jimmy <wujimmy94@gmail.com>
2019-02-03 20:34:11 -0500

    comments

--- app/assets/javascripts/components/components/Counter.js
+++ app/assets/javascripts/components/components/Counter.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 
 class Counter extends Component {
   render() {
-    const { increment, incrementIfOdd, incrementAsync, decrement, counter } = this.props;
+    const { increment, incrementIfOdd, incrementAsync, decrement, counter, resetCounter } = this.props;
     return (
       <p>
         Clicked: {counter} times
@@ -14,6 +14,8 @@ class Counter extends Component {
         <button onClick={incrementIfOdd}>Increment if odd</button>
         {' '}
         <button onClick={() => incrementAsync()}>Increment async</button>
+        {' '}
+        <button onClick={resetCounter}>Reset</button>
       </p>
     );
   }
@@ -24,6 +26,7 @@ Counter.propTypes = {
   incrementIfOdd: PropTypes.func.isRequired,
   incrementAsync: PropTypes.func.isRequired,
   decrement: PropTypes.func.isRequired,
+  resetCounter: PropTypes.func.isRequired,
   counter: PropTypes.number.isRequired
 };
 
--- app/assets/javascripts/components/containers/CounterApp.js
+++ app/assets/javascripts/components/containers/CounterApp.js
@@ -1,8 +1,10 @@
-import { bindActionCreators } from 'redux'; //Turns an object whose values are action creators, into an object with the same keys, but with every action creator wrapped into a dispatch call so they may be invoked directly.
+import { bindActionCreators } from 'redux'; // Turns an object whose values are action creators, into an object with the same keys, but with every action creator wrapped into a dispatch call so they may be invoked directly.
 import { connect } from 'react-redux'; // connects a React component to a Redux store
 import Counter from '../components/Counter';
 import * as CounterActions from '../actions/counter';
 
+
+// mapStateToProps / mapDispatchToProps explanation: https://medium.com/ovrsea/mapstatetoprops-and-why-you-may-not-need-mapdispatchtoprops-as-a-beginner-dd012a3da5e6
 function mapStateToProps(state) {
   return {
     counter: state.counter
